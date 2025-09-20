368188c4936dcd62dc8975fc41c2f89883ec55e5
Lucas Rodrigues Seidel Silva <lucas.rodrigues28012003@gmail.com>
2025-09-19 21:47:52 -0300

    feat: Adds component structure and routing for the welcome page
    Introduces a new welcome page with a form for user input, allowing navigation to the clients page upon submission.
    
    Implements structured UI components including buttons and input fields, utilizing Radix UI for accessible components.
    
    Updates the configuration to support module path aliases and enhances tailwind CSS integration.
    
    Establishes routing with React Router for better navigation flow.

---
+++ src/components/WelcomeForm/index.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { useNavigate } from "react-router-dom";
+
+const WelcomeForm = () => {
+  const navigate = useNavigate();
+
+  const [userName, setUserName] = useState<string>();
+
+  return (
+    <div className="flex flex-col space-y-2.5">
+      <h1 className="text-3xl text-center">Olá, seja bem-vindo! </h1>
+
+      <Input
+        placeholder="Digite seu nome:"
+        className="placeholder:text-stone-300 outline-0 focus:border-orange-color w-lg rounded-xs border"
+        onChange={(e) => setUserName(e.target.value)}
+      />
+
+      <Button
+        onClick={() => navigate("/clientes", { state: userName })}
+        className="bg-orange-color rounded-xs hover:bg-orange-color hover:cursor-pointer active:scale-90"
+      >
+        Entrar
+      </Button>
+    </div>
+  );
+};
+
+export default WelcomeForm;
---
+++ src/pages/Welcome/index.tsx
@@ -0,0 +1,11 @@
+import WelcomeForm from "../../components/WelcomeForm";
+
+const WelcomePage = () => {
+  return (
+    <div className="flex w-screen h-screen justify-center items-center bg-background">
+      <WelcomeForm />
+    </div>
+  );
+};
+
+export default WelcomePage;
